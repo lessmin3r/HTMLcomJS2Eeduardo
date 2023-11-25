f655c5d3e3eb52baf3d6b4935f9e8652cfdba758
lessmin3r <127457643+lessmin3r@users.noreply.github.com>
2023-11-24 22:28:10 -0300

    Update main.js
    kj

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+const tecla = listaDeTeclas[contador];
+const instrumento = tecla.classList[1];
+const idAudio = `#som_${instrumento}`; //template string
+
+tecla.onclick = function () {
+    tocaSom(idAudio);
 }
 
-document.querySelectorAll('.tecla')
+tecla.onkeydown = function (evento) {
 
+   console.log(evento.code == 'Space')
+
+   if (evento.code === 'Space') {
+    tecla.classList.add('ativa');
+   }
+
+   if (evento.code === 'Enter') {
+    tecla.classList.add('ativa');
+   }
+
+}
+
+tecla.onkeyup = function (){
+    tecla.classList.remove ('ativa');
+}
